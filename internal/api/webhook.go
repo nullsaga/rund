6e1042cc35f8ed87a6b5e9eb6100bf354644299c
nullsaga <9040460+nullsaga@users.noreply.github.com>
2025-03-23 08:35:48 +0200

    cleanup

--- internal/api/webhook.go
+++ internal/api/webhook.go
@@ -1,12 +1,6 @@
 package api
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/hex"
-	"fmt"
-	"io"
-	"log"
 	"net/http"
 )
 
@@ -17,20 +11,5 @@ func NewWebhookHandler() *WebhookHandler {
 }
 
 func (h *WebhookHandler) Handle(w http.ResponseWriter, r *http.Request) error {
-	// Infer type by headers sent
-	signature := r.Header.Get("x-hub-signature-256")
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer r.Body.Close()
-
-	mac := hmac.New(sha256.New, []byte("belekas"))
-	mac.Write(body)
-	expectedSig := mac.Sum(nil)
-	fmt.Println("expected:", hex.EncodeToString(expectedSig))
-	fmt.Println("received:", signature)
-	// hmac.Equal([]byte(expectedSignature), []byte(signature))
 	return nil
 }
